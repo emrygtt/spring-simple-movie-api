8aa3cb02c19621816946ac1b81237bbb5d455849
emrygtt <emryigit97@gmail.com>
2023-08-23 11:14:31 +0300

    movies api with spring boot

---
+++ src/main/java/dev/emrygtt/movies/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.emrygtt.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
